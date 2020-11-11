bb388d3eabc82d8e4e30448f3c0baf2ec2166a2e
viktoria.butenko <viktoria.butenko@khk.ee>
2020-11-11 18:43:32 +0200

    Adding JS

---
+++ app.js
@@ -0,0 +1,34 @@
+// define variables
+const form = document.querySelector('#task-form');
+const taskInput = document.getElementById('task').value;
+const taskList = document.querySelector('.collection');
+// define event listeners
+// add task to list - submit button
+form.addEventListener('submit', addTask);
+
+// addTask function
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert("Add new task")
+    } else {
+        // create li
+        const li = document.createElement('li');
+        //add class
+        li.className = 'collection-icon';
+        //add input value
+        li.appendChild(document.createTextNode(taskInput.value));
+        //link for element removing
+        const link = document.createElement('a');
+        //add css link
+        link.className = 'secondary.content';
+        //add fas icon
+        link.innerHTML = '<i class="fas fa-backspace"></i>';
+        //add link into li
+        li.appendChild(link);
+        //add li into ul
+        taskList.appendChild(li);
+        //clear task input
+        taskInput.value = '';
+        e.preventDefault();
+    }
+}
